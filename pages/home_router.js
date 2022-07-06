57d4ac0e006808e50dd6c81faf3c9f773ebb22e7
Damir <demchikbig777@mail.ru>
2022-07-06 15:14:55 +0300

    babel connected to app.js

--- pages/home_router.js
+++ pages/home_router.js
@@ -1,10 +1,13 @@
 const home_router = require('express').Router();
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
+// const Home = require('../../../../WEEK_1/DAY_4/214/views/Home');
 
-// app.get('/', (req,res) => {
-//   const main = React.createElement(Home, { title: 'Express' });
-//   const html = ReactDOMServer.renderToStaticMarkup(main);
-//   res.write('<!DOCTYPE html>');
-//   res.end(html);
-// });
+
+home_router.get('/' (req, res) => {
+  const main = React.createElement(Home, { title: 'VeloProgulka.Net'});
+  const html = ReactDOMServer.renderToStaticMarkup(main);
+  res.write('<!doctype html>');
+  res.end(html);
+})
+
