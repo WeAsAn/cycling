8283d5e298e2348e35ad3d106d2c8ecb4bf7e670
Damir <demchikbig777@mail.ru>
2022-07-06 14:45:26 +0300

    created pages folder for jsx routers

---
+++ pages/home_router.js
@@ -0,0 +1,10 @@
+const home_router = require('express').Router();
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+
+// app.get('/', (req,res) => {
+//   const main = React.createElement(Home, { title: 'Express' });
+//   const html = ReactDOMServer.renderToStaticMarkup(main);
+//   res.write('<!DOCTYPE html>');
+//   res.end(html);
+// });
