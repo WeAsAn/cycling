75718b1ad1996a9de862791c0456331e6c588611
PolinaMakarenko <pollinamakarenko@gmail.com>
2022-07-08 12:44:39 +0300

    comment good

--- public/js/client.js
+++ public/js/client.js
@@ -59,21 +59,24 @@ document.mycomment?.addEventListener('submit', async (event) => {
   console.log(event.target.inputcomment.value);
   console.log(event.target.querySelector('.nav-link').innerText);
   console.log(event.target.rating.value);
-  // const res = await fetch('/new/', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({
-  //     email: event.target.inputemail.value,
-  //     password: event.target.inputpassword.value,
-  //   }),
-  // });
-  // const result = await res.json();
-  // console.log(result);
-  // if (result.status === 'ok') {
-  //   window.location.assign('/home');
-  // } else {
-  //   document.querySelector('.errorMessage').innerText = result.errorMessage;
-  // }
+  console.log(event.target.getAttribute('id'));
+  const res = await fetch(`/new/addcomment`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      // user_id: event.target.querySelector('.nav-link').innerText,
+      roure_id: event.target.getAttribute('id'),
+      comment: event.target.inputcomment.value,
+      rating: event.target.rating.value,
+    }),
+  });
+  const result = await res.json();
+  console.log(result);
+  if (result.status === 'ok') {
+    window.location.assign(`/routes/${event.target.getAttribute('id')}`);
+  } else {
+    document.querySelector('.errorMessage').innerText = result.errorMessage;
+  }
 });
--- routes/views/CommentRouter.js
+++ routes/views/CommentRouter.js
@@ -7,7 +7,9 @@ const Comment = require('../../views/Comment');
 CommentRouter.get('/:id', (req, res) => {
     const { user } = req.session;
     console.log(user);
-    const com = React.createElement(Comment, {user});
+    const routeId = req.params
+    console.log(routeId);
+    const com = React.createElement(Comment, {user,routeId });
     const html = ReactDOMServer.renderToStaticMarkup(com);
     res.write('<!doctype html>');
     res.end(html);
